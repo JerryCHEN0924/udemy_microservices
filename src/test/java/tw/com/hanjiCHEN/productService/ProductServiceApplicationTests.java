580a1ad01a954d11bec058bba82281f2e7506ced
JerryCHEN <jk2455892@gmail.com>
2024-09-23 15:33:12 +0800

    init commit

---
+++ src/test/java/tw/com/hanjiCHEN/productService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.hanjiCHEN.productService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
